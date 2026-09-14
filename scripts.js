8d9403d5444e272e8e59e5e3f8389bb161ef5c3f
Abner Nteteubaka <anteteubaka@gmail.com>
2026-09-14 23:32:53 +0000

    Ajout de la fonctionnalité pour ajouter des tâches à la liste avec un bouton de suppression

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 const taskInput = document.getElementById("taskInput");
 const addTask = document.getElementById("addTask")
 const taskList = document.getElementById("taskList")
+
+addTask.addEventListener("click",() =>{
+    const task = document.createElement("li")
+    task.textContent = taskInput.value 
+    taskList.append(task)
+    const  removeTask = document.createElement("button")
+    removeTask.textContent = "Supprimer"
+    task.append(removeTask)
